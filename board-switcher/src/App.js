ab6092efc0cee676d51b31a7844f591612b4b191
pocketdru <pocketdru@gmail.com>
2019-07-13 19:01:22 -0600

    home page builded

--- board-switcher/src/App.js
+++ board-switcher/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import BoardSwitcher from "./pages/BoardSwitcher/BoardSwitcher";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+            <Route exact path="/BoardSwitcher" component={BoardSwitcher} />
+            {/* <Route exact path="/order" component={Order} /> */}
+        </Switch>
+    </Router>
+    <a href="/BoardSwitcher">
+      <button>Board Switcher</button>
+    </a>
     </div>
   );
 }
---
+++ board-switcher/src/pages/BoardSwitcher/BoardSwitcher.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+function BoardSwitcher() {
+    return (
+        <div>
+            <h1>HALLO</h1>
+
+        </div>
+    )
+}
+ 
+export default BoardSwitcher;
